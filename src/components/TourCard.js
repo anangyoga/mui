86c1eac056c286d89cecc9d38949c9df42e43d9c
anangyoga <aharwinto@gmail.com>
2022-03-20 15:41:32 +0700

    feat: use createTheme and themeProvider to style font

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -1,56 +1,82 @@
-import { Rating, Box, Grid, Paper, Typography } from "@mui/material";
+import { Rating, Box, Grid, Paper, Typography, createTheme, ThemeProvider } from "@mui/material";
 import { AccessTime } from "@mui/icons-material";
 
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: {
+            variant: "body2",
+          },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: {
+            variant: "body3",
+          },
+          style: {
+            fontSize: 9,
+          },
+        },
+      ],
+    },
+  },
+});
 const TourCard = () => {
   return (
     <Grid item xs={3}>
-      <Paper elevation={3}>
-        <img className="img" alt="" src="https://ychef.files.bbci.co.uk/1600x900/p04kt0s1.webp"></img>
-        <Box paddingX={1}>
-          <Typography variant="subtitle1" component="h2">
-            Immerse into the Falls
-          </Typography>
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img className="img" alt="" src="https://ychef.files.bbci.co.uk/1600x900/p04kt0s1.webp"></img>
+          <Box paddingX={1}>
+            <Typography variant="subtitle1" component="h2">
+              Immerse into the Falls
+            </Typography>
 
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <AccessTime
+            <Box
               sx={{
-                width: 12.3,
+                display: "flex",
+                alignItems: "center",
               }}
-            />
-            <Typography variant="body2" component="p" marginLeft={0.5}>
-              5 hours
-            </Typography>
-          </Box>
+            >
+              <AccessTime
+                sx={{
+                  width: 12.3,
+                }}
+              />
+              <Typography variant="body2" component="p" marginLeft={0.5}>
+                5 hours
+              </Typography>
+            </Box>
 
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              marginTop: 2,
-            }}
-          >
-            <Rating name="read-only" value={4.5} precision={0.5} readOnly size="small" />
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: 2,
+              }}
+            >
+              <Rating name="read-only" value={4.5} precision={0.5} readOnly size="small" />
 
-            <Typography variant="body2" component="p" marginLeft={1.5}>
-              4.5
-            </Typography>
-            <Typography variant="body3" component="p" marginLeft={1.5}>
-              (460 Reviews)
-            </Typography>
-          </Box>
+              <Typography variant="body2" component="p" marginLeft={1.5}>
+                4.5
+              </Typography>
+              <Typography variant="body3" component="p" marginLeft={1.5}>
+                (460 Reviews)
+              </Typography>
+            </Box>
 
-          <Box>
-            <Typography variant="h6" component="h3" marginTop={0}>
-              From C $147
-            </Typography>
+            <Box>
+              <Typography variant="h6" component="h3" marginTop={0}>
+                From C $147
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
